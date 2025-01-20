f66a6bff54bf84d0daf70d3f123f0c175e187875
Andrew Hill <contact@andrewthomashill.co.uk>
2025-01-20 09:18:58 +0100

    Reverse

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,13 @@ export default {
     const isExporting = url.searchParams.get('isExporting');
     const authToken = url.searchParams.get('authToken');
 
+    if (authToken !== env.AUTH_TOKEN) {
+      return new Response('Unauthorized', {
+        status: 401,
+        headers: { 'content-type': 'text/plain' },
+      });
+    }
+
     if (url.href.includes('favicon.ico')) {
       return new Response('', {
         status: 200,
